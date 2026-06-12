f96b621f513425cfb5be5a48a6e21e6305b30ede
abiqnurmagedov17 <abiqnurmagedov@gmail.com>
2026-06-13 05:16:39 +0700

    Create api/messages.js

---
+++ api/messages.js
@@ -0,0 +1,33 @@
+const { getMessages } = require('./lib/temp-mail');
+
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-cache');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Use GET method' });
+  }
+
+  const { email } = req.query;
+
+  if (!email) {
+    return res.status(400).json({
+      success: false,
+      error: 'Parameter "email" wajib diisi'
+    });
+  }
+
+  try {
+    const result = await getMessages(email);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  }
+};
